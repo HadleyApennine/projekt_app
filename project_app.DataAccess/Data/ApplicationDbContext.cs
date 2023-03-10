30deec2f3bcf86cb0a04bdbeee3af8e15090da24
Karina Królikowska <Karina.Krolikowska@microsoft.wsei.edu.pl>
2023-03-10 19:24:05 +0100

    naprawione odwołania

--- project_app.DataAccess/Data/ApplicationDbContext.cs
+++ project_app.DataAccess/Data/ApplicationDbContext.cs
@@ -1,13 +1,15 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using Microsoft.Build.Framework.XamlTypes;
+using Microsoft.EntityFrameworkCore;
+using project_app.Models;
 using projekt_app.Models;
 
-namespace projekt_app.Data
+namespace projekt_app.DataAccess
 {
     public class ApplicationDbContext : DbContext
     {
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
         {
-            
+            //
         }
 
         public DbSet<Category> Categories { get; set; }
---
+++ projekt_app.Models/Models/Category.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace projekt_app.Models
+{
+    public class Category
+    {
+        // Server name: DESKTOP-CMMD1H6
+        [Key]
+
+        public int Id { get; set; }
+        [Required]
+        public string Name { get; set; }
+        [DisplayName("Display Order")]
+        [Range(1, 100, ErrorMessage ="Display order can only be between 1 and 100.")]
+        public string DisplayOrder { get; set; }
+        public DateTime CreatedDateTime { get; set; } = DateTime.Now;
+    }
+}
